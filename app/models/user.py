deef22d1ba1cc54e6145d29cde7dc1f7517442f0
Diego Regalado <alejandroregaladotangarife@gmail.com>
2026-07-27 19:10:25 -0500

    add updated_at field to users

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,8 @@
 from datetime import datetime,timezone
+
 from sqlalchemy import String, DateTime
 from sqlalchemy.orm import Mapped, mapped_column
+
 from app.database.database import Base
 
 class User(Base):
@@ -8,10 +10,14 @@ class User(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
-    email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
     hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
     created_at: Mapped[datetime] = mapped_column(
         DateTime(), 
         default=lambda: datetime.now(timezone.utc), 
         nullable=False)
-
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(), 
+        default=lambda: datetime.now(timezone.utc), 
+        onupdate=lambda: datetime.now(timezone.utc),
+        nullable=False)
